d7496ff65cf1e58378ac3ebf3e86fb1894c1148f
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-02-26 15:57:06 +0100

    Add files via upload

---
+++ freelancers_DB.sql
@@ -0,0 +1,11 @@
+CREATE TABLE freelancers (
+	id INT PRIMARY KEY,
+	first_name VARCHAR(120),
+	last_name VARCHAR (120),
+	email VARCHAR (120)
+	
+);
+INSERT INTO freelancers (id, first_name, last_name, email) VALUES (1, 'Pete', 'Thompson', 'pthompson@gmail.com');
+INSERT INTO freelancers (id, first_name, last_name, email) VALUES (2, 'Nadine', 'Lopez', 'nlopez@gmail.com');
+INSERT INTO freelancers (id, first_name, last_name, email) VALUES (3, 'Claudio', 'Stratos', 'cstratos@gmail.com');
+INSERT INTO freelancers (id, first_name, last_name, email) VALUES (4, 'Miriam', 'Valetti', 'mvaletti@gmail.com');
